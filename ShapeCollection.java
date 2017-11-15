9b3afac325a0a4f14083f9851bc68f70ed76da47
Trin Sengchareon <melonn.qq@gmail.com>
2017-11-15 20:57:17 +0700

    Part E complete

--- ShapeCollection.java
+++ ShapeCollection.java
@@ -6,15 +6,29 @@ public class ShapeCollection {
 		// create all shape objtects
 		// add all objects to ArrayList
 		// compute total area and draw it
+		Shape t1 = new Triangle(7,0,'*');
+		Shape s1 = new Square(5,4,'&');
+		Shape t2 = new Triangle(7,0,'*',true);
+		upDownArrow.add(t1);
+		upDownArrow.add(s1);
+		upDownArrow.add(t2);
+
+		System.out.println("the total area is " + computeArea(upDownArrow));
+		draw(upDownArrow);
 	}
 
 	public static double computeArea(ArrayList<Shape> shapeList) {
 		double totalArea = 0;
-		// fill code
+
+		for(int i=0;i<shapeList.size();i++){
+			totalArea += shapeList.get(i).computeArea();
+		}
 		return totalArea;
 	}
 
 	public static void draw(ArrayList<Shape> shapeList) {
-		// fill code
+		for(int i=0;i<shapeList.size();i++){
+			shapeList.get(i).draw();
+		}
 	}
 }
